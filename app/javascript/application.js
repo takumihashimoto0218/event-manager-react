e90f83a6d46d3bb66b68080036a5c9af1c194306
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-05 16:58:58 +0900

    hello-worldを表記

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,3 +13,13 @@
 //
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import HelloMessage from './components/App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(<HelloMessage name="World" />);
+});
